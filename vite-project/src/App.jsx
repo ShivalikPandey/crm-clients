5c17abeb9b5ff9f11c7a89d2f06601ca43d3393b
Shivalik-pandey <shivalik@oneplacesolar.com>
2026-06-18 23:54:40 +0530

    add tailwind in project

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,10 @@
+import Login from "./components/login"
+
 function App() {
   return (
     <div className="App">
       <h1>Hello Vite!</h1>
+      <Login />
     </div>
   )
 }
---
+++ vite-project/src/components/login.jsx
@@ -0,0 +1,8 @@
+function Login() {
+    return (
+        <>
+        <p>Login Component</p>
+        </>
+    )
+}
+export default Login
